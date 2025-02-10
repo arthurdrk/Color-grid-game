650e9cc6776eaa5d8df6bb16d5b919133ccdcbc9
arthurdrk <arthr.drck@gmail.com>
2025-02-10 18:10:04 +0100

    update

--- code/main.py
+++ code/main.py
@@ -1,18 +1,17 @@
 from grid import Grid
 from solver import *
 
-grid = Grid(2, 3)
-print(grid)
 
 data_path = "C:/Users/arthr/Desktop/ENSAE 1A/S2/Projet-Prog/Projet-de-programmation-1A/input/"
-
 file_name = data_path + "grid01.in"
+
 grid = Grid.grid_from_file(file_name, read_values=True)
 print(grid)
-grid.plot()
+
 
 
 solver = SolverBiparti(grid)
 solver.run()
 print("The final score of SolverBiparti is:", solver.score())
 print(solver.pairs)
+grid.plot()
