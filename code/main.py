4bf5552fcff8121bdc861d858ca5ca18a407cacd
arthurdrk <arthr.drck@gmail.com>
2025-02-10 18:06:53 +0100

    rename

--- code/main.py
+++ code/main.py
@@ -7,18 +7,10 @@ print(grid)
 data_path = "C:/Users/arthr/Desktop/ENSAE 1A/S2/Projet-Prog/Projet-de-programmation-1A/input/"
 
 file_name = data_path + "grid01.in"
-grid = Grid.grid_from_file(file_name)
-print(grid)
-
-file_name = data_path + "grid01.in"
 grid = Grid.grid_from_file(file_name, read_values=True)
 print(grid)
 grid.plot()
 
-solver = SolverEmpty(grid)
-solver.run()
-print("The final score of SolverEmpty is:", solver.score())
-print(solver.pairs)
 
 solver = SolverBiparti(grid)
 solver.run()
--- code/solver.py
+++ code/solver.py
@@ -49,90 +49,7 @@ class SolverEmpty(Solver):
 class SolverGreedy(Solver):
     def run(self):
         pass
-
-# class SolverBiparti(Solver):
-#     def run(self):
-#         # Supposons que self.grid.all_pairs() renvoie toutes les paires de cellules adjacentes.
-#         # On veut construire un graphe biparti : cellules paires à gauche, cellules impaires à droite.
-#         # Puis, ajouter une source "s" reliée à toutes les cellules paires et un puits "t" relié à toutes les cellules impaires.
-#         graph = {}
-#         even_cells = set()
-#         odd_cells = set()
-#         # Ajout des arêtes entre cellules (direction : de pair vers impair)
-#         for cell1, cell2 in self.grid.all_pairs():
-#             # Déterminer laquelle est paire (i+j pair) et laquelle est impaire.
-#             if sum(cell1) % 2 == 0:
-#                 even = cell1
-#                 odd  = cell2
-#             else:
-#                 even = cell2
-#                 odd  = cell1
-#             even_cells.add(even)
-#             odd_cells.add(odd)
-#             # Ajouter l'arête de la cellule paire vers la cellule impaire.
-#             graph.setdefault(even, []).append(odd)
-        
-#         # Ajout des arêtes de la source "s" aux cellules paires,
-#         # et des cellules impaires vers le puits "t".
-#         for even in even_cells:
-#             graph.setdefault("s", []).append(even)
-#             # S'assurer que chaque cellule paire apparaisse dans le graphe même si elle n'a pas d'arêtes sortantes.
-#             graph.setdefault(even, [])
-#         for odd in odd_cells:
-#             graph.setdefault(odd, []).append("t")
-#             graph.setdefault(odd, [])
-#         graph.setdefault("t", [])
-        
-#         # Conserver les ensembles de cellules paires et impaires pour la récupération ultérieure de l'appariement.
-#         even_cells = even_cells
-#         odd_cells = odd_cells
-#         self.ford_fulkerson(graph)
-        
-        
-#     def dfs(self,graph, u, t, visited):
-#         if u == t:
-#             return [t]
-#         for v in graph.get(u, []):
-#             if v not in visited:
-#                 visited.add(v)
-#                 path = self.dfs(graph,v, t, visited)
-#                 if path is not None:
-#                     return [u] + path
-#         return None
-        
-#     def ford_fulkerson(self,graph):
-#         """
-#         Calcule le flot maximum dans self.graph (le réseau d'appariement biparti)
-#         en utilisant une recherche DFS pour trouver des chemins augmentants (algorithme Ford–Fulkerson).
-
-#         Renvoie:
-#             max_flow (int) : la valeur du flot maximum, c'est-à-dire la taille de l'appariement maximum.
-#             appariements (list) : une liste de tuples (cellule_pair, cellule_impair) représentant l'appariement.
-#         """
-#         max_flow = 0
-#         # Recherche répétée d'un chemin augmentant de "s" à "t"
-#         while True:
-#             visited = set(["s"])
-#             path = self.dfs(graph,"s", "t", visited)
-#             if path is None:
-#                 break  # plus de chemin augmentant trouvé
-#             max_flow += 1
-#             # Pour des capacités unitaires, le goulot d'étranglement est toujours 1.
-#             # Mise à jour du graphe résiduel :
-#             # pour chaque arête du chemin, on retire l'arête dans le sens direct
-#             # et on ajoute l'arête inverse.
-#             for i in range(len(path) - 1):
-#                 u = path[i]
-#                 v = path[i + 1]
-#                 graph[u].remove(v)
-#                 graph.setdefault(v, []).append(u)
-#         # Récupérer l'appariement à partir du graphe résiduel.
-#         # Dans ce réseau, une arête de "v" (cellule impaire) vers "u" (cellule paire)
-#         # indique que la cellule paire u est appariée avec la cellule impaire v.
-#         appariements = [(u, v) for v in graph for u in graph[v]]
-#         print(path)
-
-
+    
 class SolverBiparti(Solver):
     def run(self):
         # On suppose que self.grid.all_pairs() renvoie toutes les paires de cellules adjacentes.
@@ -175,10 +92,8 @@ class SolverBiparti(Solver):
         self.even_cells = even_cells
         self.odd_cells = odd_cells
         # Calculer le flot maximum (donc la taille de l'appariement maximum)
-        max_flow, matching = self.ford_fulkerson(graph)
-        print("Max flow:", max_flow)
-        print("Matching:", matching)
-        return max_flow, matching
+        matching = self.ford_fulkerson(graph)
+        return matching
     
     
     def dfs(self, graph, u, t, visited):
