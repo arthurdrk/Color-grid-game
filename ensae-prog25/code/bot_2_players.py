8cf7ac2aef8092aa46a9317ddc073de72424afea
Dimtri <66122343+diabitbol@users.noreply.github.com>
2025-03-24 21:01:03 +0100

    Update bot_2_players.py

--- ensae-prog25/code/bot_2_players.py
+++ ensae-prog25/code/bot_2_players.py
@@ -1,9 +1,10 @@
 from grid import Grid
 
+
 def move_to_play(grid: Grid):
     """
-    Choose the best pair considering that the opponent is playing greedy and wants to choose the best pair possible
-    with a prediction of one turn.
+    Choose the best pair considering that the opponent is playing greedy and
+    will choose the best possible pair with a one-turn prediction.
 
     Parameters:
     -----------
@@ -13,43 +14,49 @@ def move_to_play(grid: Grid):
     Returns:
     --------
     pair : tuple[tuple[int, int], tuple[int, int]]
-        The pair of cells to be played, or None if no choice is possible.
+        The pair of cells to be played,
+        or None if no choice is possible.
 
-    Complexity: O((n*m)²) can be bettered to O(n*m).
-    """
+    Complexity : O(n*m * log(n*m))
+    """ 
 
-    pairs = list(set(sorted(grid.all_pairs())))
-    res = None  # tuple[tuple[int, int], tuple[int, int]]
-    best = float('inf')  # best score with this choice of pair
+    pairs = grid.all_pairs()  # O(n*m)
+    if not pairs:
+        return None
 
-    # Sort pairs by cost
-    pairs.sort(key=lambda pair: grid.cost(pair))
+    # 1) We sort all pairs by cost (from smallest to largest)
+    pairs_sorted = sorted(pairs, key=lambda p: grid.cost(p))  # O(n*m * log(n*m))
 
-    if len(pairs) == 1:
-        return pairs[0]
-    else:
-        for pair in pairs:
-            grid_copy = Grid(grid.n, grid.m, grid.color, grid.value)
-            grid_copy.color[pair[0][0]][pair[0][1]] = 4
-            grid_copy.color[pair[1][0]][pair[1][1]] = 4
+    best_score = float('inf')
+    best_pair_for_us = None
 
-            # Assume the opponent plays greedy and find the best pair remaining without pair
-            pairs2 = grid_copy.all_pairs()
+    # We iterate over all possible pairs (O(n*m) iterations)
+    for pair in pairs:
+        # We "block" the two cells in this pair
+        used_cells = {pair[0], pair[1]}  
 
-            if not pairs2:
-                # If there are no pairs left, skip this iteration
-                continue
+        # 2) Find the best possible pair for the opponent
+        #    by going through pairs_sorted
+        choice_adversaire = None
+        for candidate in pairs_sorted:  # In practice, we skip quickly if adjacency is limited
+            c0, c1 = candidate
+            if c0 not in used_cells and c1 not in used_cells:
+                # This pair is free in the modified grid
+                choice_adversaire = candidate
+                break
+        # If we didn't find any free pair => the opponent can't play
+        # We can set cost=0 or "no impact from the opponent"
+        
+        if choice_adversaire is None:
+            # The opponent does not play
+            score = 0 - grid.cost(pair)
+        else:
+            score = grid.cost(choice_adversaire) - grid.cost(pair)
 
-            player_choice = min([grid_copy.cost(x) for x in pairs2])
-            score = grid.cost(pair) - player_choice
+        # We take the pair that minimizes the score
+        if score < best_score:
+            best_score = score
+            best_pair_for_us = pair
 
-            if score < best:
-                best = score
-                res = pair
-
-            # If we find a pair with cost 0, return it immediately
-            if grid.cost(pair) == 0:
-                return pair
-
-        return res
+    return best_pair_for_us
 
